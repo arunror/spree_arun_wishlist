df70b75729072bfdde272bca39b4ecefa06d2586
arunror <rise2arun@gmail.com>
2015-05-11 14:12:46 +0530

    First commit

---
+++ db/migrate/20150504082759_create_spree_wishlists.rb
@@ -0,0 +1,11 @@
+class CreateSpreeWishlists < ActiveRecord::Migration
+  def change
+    create_table :spree_wishlists do |t|
+    	t.references :user
+      	t.references :product
+
+      	t.timestamps
+
+    end
+  end
+end
