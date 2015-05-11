62b9479a24c9c6502c4b9a51bf143263439daaf1
arunror <rise2arun@gmail.com>
2015-05-11 16:17:45 +0530

    Second commit

--- app/overrides/add_wishlistlink_before_myaccount.rb
+++ app/overrides/add_wishlistlink_before_myaccount.rb
@@ -2,5 +2,6 @@ Deface::Override.new(
   :virtual_path => "spree/shared/_main_nav_bar",
   :name => "add_wishlist_link",
   :insert_after => "li#link-to-cart",
+  :original => 'e9241f273349c0f080cef375ce5d2c26546eb870'
   :partial => "spree/shared/wishlist_link"
 )
